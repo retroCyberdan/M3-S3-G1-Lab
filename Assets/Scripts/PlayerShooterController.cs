c50bd3671152aab7c9fb62a7bb5286a11e9d26a4
retroCyberdan <105318240+retroCyberdan@users.noreply.github.com>
2025-05-29 18:14:49 +0200

    Update PlayerShooterController.cs

--- Assets/Scripts/PlayerShooterController.cs
+++ Assets/Scripts/PlayerShooterController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerShooterController : MonoBehaviour
 {
-    {SerializeField] private float _fireRate;
+    [SerializeField] private float _fireRate;
     [SerializeField] private float _fireRange;
 
     
