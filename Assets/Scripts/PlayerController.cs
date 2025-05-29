5882bd08864dee8bc1779a797efe7a404b36b619
retroCyberdan <105318240+retroCyberdan@users.noreply.github.com>
2025-05-29 18:13:56 +0200

    Add exercise 1 + scripts

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private float _speed = 10;
+    [SerializeField] Rigidbody2D _rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        if(h != 0 || v != 0)
+        {
+            Vector2 dir = new Vector2(h, v); // <- creo un vettore direzione
+
+            float length = dir.magnitude; // <- calcolo la sua lunghezza
+
+            if(length > 1)
+            {
+                dir /= length; // <- normalizzo il vettore se la lunghezza è > di 1
+            }
+
+            _rb.MovePosition(_rb.position + dir * (_speed * Time.deltaTime)); // <- eseguo il movimento tramite Rigidbody2D
+        }
     }
 }
--- Assets/Scripts/PlayerShooterController.cs
+++ Assets/Scripts/PlayerShooterController.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class PlayerShooterController : MonoBehaviour
 {
+    {SerializeField] private float _fireRate;
+    [SerializeField] private float _fireRange;
+
+    
     // Start is called before the first frame update
     void Start()
     {
